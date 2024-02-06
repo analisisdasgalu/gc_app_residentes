ee9cda63235aeb2e9ba7ee2c08ad9ed3741ebe40
Marco Fuentes <marco.fuentes.teseracto@smartbusinesscorp.com>
2024-02-05 23:46:36 -0600

    Saving and Sharing QR code

--- src/screens/QRDetails/QRDetails.tsx
+++ src/screens/QRDetails/QRDetails.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { TouchableOpacity, View, Text, Image } from "react-native";
+import {
+	TouchableOpacity,
+	View,
+	Text,
+	Image,
+	Platform,
+	PermissionsAndroid,
+	Alert,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import ViewShot, { captureRef } from "react-native-view-shot";
@@ -8,6 +16,9 @@ import { colors } from "@gcMobile/theme/default.styles";
 import { useSelector } from "react-redux";
 import { RootState } from "@gcMobile/store";
 import Share from "react-native-share";
+import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+import { ALERT_TYPE, Toast } from "react-native-alert-notification";
+import RNFetchBlob from "rn-fetch-blob";
 
 type QRDetailsProps = {
 	navigation: any;
@@ -31,6 +42,79 @@ export const QRDetails = ({ route, navigation }: QRDetailsProps) => {
 			await Share.open({ url: uri });
 		} catch (e) {
 			console.log(e);
+		}
+	};
+
+	async function hasAndroidPermission() {
+		const getCheckPermissionPromise = () => {
+			if (Platform.Version >= `33`) {
+				return Promise.all([
+					PermissionsAndroid.check(
+						PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES
+					),
+					PermissionsAndroid.check(
+						PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO
+					),
+				]).then(
+					([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+						hasReadMediaImagesPermission && hasReadMediaVideoPermission
+				);
+			} else {
+				return PermissionsAndroid.check(
+					PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+				);
+			}
+		};
+
+		const hasPermission = await getCheckPermissionPromise();
+		if (hasPermission) {
+			return true;
+		}
+		const getRequestPermissionPromise = () => {
+			if (Platform.Version >= `33`) {
+				return PermissionsAndroid.requestMultiple([
+					PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+					PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+				]).then(
+					(statuses) =>
+						statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+							PermissionsAndroid.RESULTS.GRANTED &&
+						statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+							PermissionsAndroid.RESULTS.GRANTED
+				);
+			} else {
+				return PermissionsAndroid.request(
+					PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+				).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
+			}
+		};
+
+		return await getRequestPermissionPromise();
+	}
+
+	const saveToCameraRoll = async () => {
+		if (Platform.OS === "android" && !(await hasAndroidPermission())) {
+			return;
+		}
+		try {
+			RNFetchBlob.config({
+				fileCache: true,
+				appendExt: "png",
+			})
+				.fetch("GET", url)
+				.then((res) => {
+					CameraRoll.saveAsset(res.data, { type: "photo" })
+						.then((res) => {
+							Alert.alert("Informacion", "El QR se ha guardo correctamente");
+						})
+						.catch((err) => console.log(err));
+				})
+				.catch((error) => console.log(error));
+		} catch (error) {
+			Toast.show({
+				type: ALERT_TYPE.DANGER,
+				textBody: "No se pudo guardar la imagen",
+			});
 		}
 	};
 
@@ -102,7 +186,8 @@ export const QRDetails = ({ route, navigation }: QRDetailsProps) => {
 							width: "10%",
 							margin: "auto",
 							alignItems: "center",
-						}}>
+						}}
+						onPress={() => saveToCameraRoll()}>
 						<Entypo name='download' size={24} color='black' />
 					</TouchableOpacity>
 				</View>
