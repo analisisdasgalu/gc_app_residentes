297c4df8038addbfaac264e4cb024f7d14a5f28b
MFuentes1992 <markfuentes2991@gmail.com>
2024-08-11 14:13:31 -0600

    Finishing Home View styles

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,50 +1,66 @@
 import React from 'react'
 import { ScrollView } from 'react-native-gesture-handler'
-import { HomeCardProps, HomeCreateVisitProps } from './types'
+import { HomeCardProps, HomeCreateVisitProps, LastPaymentInformationProps, NotificationCardProps } from './types'
 import { View, Text, Image } from 'react-native'
-import { add_visit_button_styles, Generate_Visit_Styles, heade_container_styles } from './constants'
 import CircularButton from '@gcMobile/components/CircularButton'
 import { circularBtnStyles } from '@gcMobile/components/CircularButton/constants'
-import { fonts } from '@gcMobile/theme/default.styles'
+import { colors, fonts } from '@gcMobile/theme/default.styles'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import {
+    add_visit_button_styles,
+    card_container_styles,
+    Generate_Visit_Styles,
+    icons_styles,
+    Image_Styles,
+    Last_Payment_Row,
+    Main_Body_Titles,
+    Main_Info_Body,
+    Main_Info_Body_Account,
+    Main_Info_Headers,
+    row_label,
+} from './constants'
 
 const HeaderCard = (props: HomeCardProps) => {
     return (
-        <View style={[heade_container_styles]}>
-            <View>
-                {/* Header */}
-                <Image source={{ uri: props.imageUrl }} width={100} height={100} />
-                <View>
-                    <Text>{props.nombre}</Text>
-                    <Text>{props.instalacion}</Text>
+        <View style={[card_container_styles]}>
+            <View style={[Main_Info_Headers]}>
+                <Image source={{ uri: props.imageUrl }} style={[Image_Styles]} />
+                <View style={[{ paddingTop: 10 }]}>
+                    <Text style={[{ fontSize: fonts.bodyText1, fontWeight: 'bold' }]}>{props.nombre}</Text>
+                    <Text style={[{ fontSize: fonts.bodyText1, fontWeight: 'bold' }]}>{props.instalacion}</Text>
                 </View>
             </View>
-            <View>
-                {/* Body */}
-                <Text>Datos bancarios de: {props.recinto}</Text>
-                <View>
-                    <Text>Banco:</Text>
-                    <Text>{props.bankInformation.bankName}</Text>
+            <View style={[Main_Info_Body]}>
+                <View style={[Main_Info_Body_Account]}>
+                    <Text style={[Main_Body_Titles]}>Datos bancarios de {props.recinto}</Text>
+                    <View style={[row_label]}>
+                        <Text style={{ color: colors.black }}>Banco </Text>
+                        <Text style={{ marginLeft: 10, color: colors.gray }}>{props.bankInformation.bankName}</Text>
+                    </View>
+                    <View style={[row_label]}>
+                        <Text>Cuenta </Text>
+                        <Text style={{ marginLeft: 5, color: colors.gray }}>{props.bankInformation.accountNumber}</Text>
+                    </View>
+                    <View style={[row_label]}>
+                        <Text>CLABE </Text>
+                        <Text style={{ marginLeft: 5, color: colors.gray }}>{props.bankInformation.CLABE}</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text>Cuenta:</Text>
-                    <Text>{props.bankInformation.accountNumber}</Text>
-                </View>
-                <View>
-                    <Text>CLABE:</Text>
-                    <Text>{props.bankInformation.CLABE}</Text>
-                </View>
-                <Text>Referencias de pago</Text>
-                <View>
-                    <Text>Bancaria:</Text>
-                    <Text>{props.paymentReference.targetBank}</Text>
-                </View>
-                <View>
-                    <Text>Concepto:</Text>
-                    <Text>{props.paymentReference.reference}</Text>
-                </View>
-                <View>
-                    <Text>Centavos:</Text>
-                    <Text>{props.paymentReference.cents}</Text>
+                <View style={[Main_Info_Body_Account]}>
+                    <Text style={[Main_Body_Titles]}>Referencias de pago</Text>
+                    <View style={[row_label]}>
+                        <Text>Bancaria </Text>
+                        <Text style={{ marginLeft: 10, color: colors.gray }}>{props.paymentReference.targetBank}</Text>
+                    </View>
+                    <View style={[row_label]}>
+                        <Text>Concepto </Text>
+                        <Text style={{ marginLeft: 5, color: colors.gray }}>{props.paymentReference.reference}</Text>
+                    </View>
+                    <View style={[row_label]}>
+                        <Text>Centavos </Text>
+                        <Text style={{ marginLeft: 5, color: colors.gray }}>{props.paymentReference.cents}</Text>
+                    </View>
                 </View>
             </View>
         </View>
@@ -55,7 +71,12 @@ const HomeCreateVisit = (props: HomeCreateVisitProps) => {
     return (
         <View style={[Generate_Visit_Styles]}>
             <View style={[add_visit_button_styles]}>
-                <CircularButton styles={circularBtnStyles.container} window={props.window} icon={props.icon} />
+                <CircularButton
+                    styles={circularBtnStyles.container}
+                    window={props.window}
+                    icon={props.icon}
+                    color={colors.white}
+                />
             </View>
             <View style={[add_visit_button_styles]}>
                 <Text style={[{ fontSize: fonts.bodyText1 }]}>Generar visita</Text>
@@ -64,14 +85,55 @@ const HomeCreateVisit = (props: HomeCreateVisitProps) => {
     )
 }
 
+export const LastPaymentInformation = (props: LastPaymentInformationProps) => {
+    return (
+        <View style={[card_container_styles]}>
+            <View style={[Last_Payment_Row, { borderBottomColor: colors.gray, borderBottomWidth: 1 }]}>
+                <View>
+                    <Text style={[Main_Body_Titles]}>Último estado de cuenta</Text>
+                    <Text style={{ color: colors.gray }}>{props.dateAccountBalance}</Text>
+                </View>
+                <View>
+                    <MaterialCommunityIcons name="file-document-multiple-outline" style={icons_styles} />
+                </View>
+            </View>
+            <View style={[Last_Payment_Row]}>
+                <View>
+                    <Text style={[Main_Body_Titles]}>Último recibo de pago</Text>
+                    <Text style={{ color: colors.gray }}>{props.dateLastPayment}</Text>
+                </View>
+                <View>
+                    <MaterialCommunityIcons name="credit-card-edit-outline" style={icons_styles} />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export const NotificationCard = (props: NotificationCardProps) => {
+    return (
+        <View style={[card_container_styles]}>
+            <View style={[Last_Payment_Row]}>
+                <View>
+                    <Ionicons name={props.icon} size={24} color={props.iconColor} />
+                </View>
+                <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+                    <Text style={[Main_Body_Titles]}>{props.title}</Text>
+                    <Text style={{ color: colors.gray }}>{props.date}</Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
 export const Home = () => {
     return (
-        <ScrollView>
+        <ScrollView overScrollMode="never">
             <HeaderCard
                 imageUrl="https://via.placeholder.com/150"
-                nombre="Nombre"
-                recinto="Recinto"
-                instalacion="Instalacion"
+                nombre="Victor Vargas"
+                recinto="Oyamel"
+                instalacion="Ambar 01"
                 bankInformation={{
                     bankName: 'Banamex',
                     accountNumber: '1234567890',
@@ -84,6 +146,13 @@ export const Home = () => {
                 }}
             />
             <HomeCreateVisit window="VisitScreen" icon="plus" />
+            <LastPaymentInformation dateAccountBalance="Julio 2020-01-01" dateLastPayment="Julio 2020-01-01" />
+            <NotificationCard
+                title="Mantenimiento de Jardines"
+                date="2020-01-01"
+                icon="megaphone-outline"
+                iconColor={colors.gray}
+            />
         </ScrollView>
     )
 }
--- src/screens/Home/constants/index.ts
+++ src/screens/Home/constants/index.ts
@@ -1,4 +1,12 @@
-import { ViewStyle } from 'react-native'
+import { colors, fonts } from '@gcMobile/theme/default.styles'
+import { ImageStyle, TextStyle, ViewStyle } from 'react-native'
+
+export const row_label: ViewStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: '90%',
+}
 
 export const card_container_styles: ViewStyle = {
     width: '90%',
@@ -9,6 +17,47 @@ export const card_container_styles: ViewStyle = {
     backgroundColor: 'white',
     borderRadius: 10,
     elevation: 5,
+    maxHeight: 300,
+}
+
+export const Main_Info_Headers: ViewStyle = {
+    display: 'flex',
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderBottomColor: colors.lightGray,
+    borderBottomWidth: 2,
+    paddingBottom: 10,
+    marginBottom: 10,
+}
+
+export const Main_Info_Body: ViewStyle = {
+    display: 'flex',
+    width: '90%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+
+export const Main_Info_Body_Account: ViewStyle = {
+    display: 'flex',
+    width: '90%',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    marginBottom: 10,
+}
+
+export const Main_Body_Titles: TextStyle = {
+    fontSize: fonts.typingText,
+    fontWeight: 'bold',
+    marginBottom: 5,
+}
+
+export const Image_Styles: ImageStyle = {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    marginRight: 10,
 }
 
 export const Generate_Visit_Styles: ViewStyle = {
@@ -28,4 +77,18 @@ export const add_visit_button_styles: ViewStyle = {
     justifyContent: 'center',
     alignItems: 'center',
     alignContent: 'center',
+}
+
+export const Last_Payment_Row: ViewStyle = {
+    display: 'flex',
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+}
+
+export const icons_styles: any = {
+    marginLeft: '2%',
+    fontSize: 20,
+    color: colors.gray,
 }
